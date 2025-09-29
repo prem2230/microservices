4d3aae66d3cd640a915ea321edf28261a7326623
PremKumar <109847674+prem2230@users.noreply.github.com>
2025-09-29 15:07:29 +0530

    feat: integrate Kafka for user registration and login events, add Kafka service initialization and closure

---
+++ user-service/src/config/kafka.js
@@ -0,0 +1,17 @@
+import { Kafka } from 'kafkajs';
+import 'dotenv/config';
+
+const kafka = new Kafka({
+    clientId: 'user-service',
+    brokers: [process.env.KAFKA_BROKER]
+});
+
+console.log(process.env.KAFKA_BROKER);
+export const producer = kafka.producer();
+export const consumer = kafka.consumer({ groupId: 'user-service-group' });
+
+export const TOPICS = {
+    USER_REGISTERED: 'user.registered',
+    USER_LOGGED_IN: 'user.logged_in',
+    USER_UPDATED: 'user.updated'
+}
